bc7c064380a609eee8845a030cd974f1f82cd092
Julio Sansossio <J.Sansossio@payvision.com>
2020-09-05 17:19:34 +0200

    Get version from environment variable

--- libs/http/src/lib/setup-swagger.ts
+++ libs/http/src/lib/setup-swagger.ts
@@ -1,13 +1,11 @@
 import { INestApplication } from '@nestjs/common'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
-const { version } = require('../../../../package.json')
-
 export const setupSwagger = (app: INestApplication, title: string, path: string = 'api') => {
   const options = new DocumentBuilder()
     .setTitle(title)
     .addBearerAuth()
-    .setVersion(version)
+    .setVersion(process.env.npm_package_version)
     .build()
   const document = SwaggerModule.createDocument(app, options)
   SwaggerModule.setup(path, app, document)
