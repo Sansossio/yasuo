d3c15c007f3ca261b02ce1c6023d99a123797d8f
Julio Sansossio <J.Sansossio@payvision.com>
2020-09-05 19:07:42 +0200

    Add tag

--- apps/summoner/src/app/summoner/controller/summoner.controller.ts
+++ apps/summoner/src/app/summoner/controller/summoner.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get, Query } from '@nestjs/common'
 import { GetSummonerByNameDto } from '../dto/get-summoner-by-name.dto'
 import { SummonerService } from '../service/summoner.service'
+import { ApiTags } from '@nestjs/swagger'
 
 @Controller()
+@ApiTags('Summoners')
 export class SummonerController {
   constructor (
     private readonly service: SummonerService
