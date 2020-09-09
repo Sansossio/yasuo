a3c89b850c2c382cfe12f9dc55ce931b6061b983
Julio Sansossio <J.Sansossio@payvision.com>
2020-09-09 09:24:14 +0200

    Add test coverage and spectator service testing

---
+++ apps/summoner/src/app/spectator/service/spectator.service.spec.ts
@@ -0,0 +1,42 @@
+import { SpectatorService } from './spectator.service'
+import { Regions } from 'twisted/dist/constants'
+import { NotFoundException } from '@nestjs/common'
+
+describe('Spectator service', () => {
+  const spectatorApi = {
+    activeGame: jest.fn()
+  }
+  const riotApi = {
+    getLolApi: () => ({
+      Spectator: spectatorApi
+    })
+  }
+  const summonerService = {
+    getByName: jest.fn()
+  }
+  const service = new SpectatorService(
+    summonerService as any,
+    riotApi as any
+  )
+  const params = {
+    summonerName: 'Test',
+    region: Regions.AMERICA_NORTH
+  }
+
+  it('should throw NotFoundException when summoner does not exists', async () => {
+    summonerService.getByName.mockImplementation(() => Promise.resolve(null))
+
+    await expect(service.spectator(params)).rejects.toBeInstanceOf(NotFoundException)
+  })
+
+  it('should throw NotFoundException when summoner does not have an active game (property message into activeGame response)', async () => {
+    summonerService.getByName.mockImplementation(() => Promise.resolve({
+      ids: {}
+    }))
+    spectatorApi.activeGame.mockImplementation(() => ({
+      message: 'Not found'
+    }))
+
+    await expect(service.spectator(params)).rejects.toBeInstanceOf(NotFoundException)
+  })
+})
